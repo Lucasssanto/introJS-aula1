2891acdb7196f9941625146ec065ed2d2f3ebc49
Lucas Dos Santos <66095197+Lucasssanto@users.noreply.github.com>
2022-01-31 15:52:38 -0300

    introJSaula1

---
+++ 2-trabalhandoComVariaveis.js
@@ -0,0 +1,11 @@
+//Console.log("Trabalhando com variáveis");
+console.log("Trabalhando com variáveis");
+//JS é case sensive
+
+const nome = "Lucas";
+const sobrenome = "Santos";
+const idade = 25;
+const ano = 2022;
+
+//não fazer
+//mes  = "Janeiro";
