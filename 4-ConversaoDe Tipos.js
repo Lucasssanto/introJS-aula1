efd8a4b502efa2f722fdc2200f880c085790426f
Lucas Dos Santos <66095197+Lucasssanto@users.noreply.github.com>
2022-01-31 18:34:00 -0300

    introJS-aula1

--- 4-ConversaoDe Tipos.js
+++ 4-ConversaoDe Tipos.js
@@ -3,3 +3,9 @@ console.log("Conversão de Tipos");
 console.log("Ano" + 2022);
 console.log("2" + "2");
 console.log(parseInt("2") + parseInt("2"));
+
+console.log("10" / "2");
+console.log("7" / "2");
+console.log("Ricardo" / "2"); //Not a Number
+
+console.log(3.5);
---
+++ 5-AtribuicaoVariaveis.js
@@ -0,0 +1,14 @@
+console.log("Trabalhando com atribuição de variáveis");
+const idade = 25;
+const primerioNome = "Lucas";
+let sobrenome = "Santos";
+
+//console.log(nome + " " + sobrenome);
+console.log(primeiroNome, sobrenome);
+conseole.log('Meu nome é ${nome} ${sobrenome}');
+
+let contador = 0
+contador = contador + 1
+nomeCompleto = primeiroNome + sobrenome;
+console.log(nomeCompleto);
+nomeCompleto = 2;
