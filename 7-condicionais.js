c1f8a59b7a55b8f9acdc67f89834db0228d2e5df
Lucas Dos Santos <66095197+Lucasssanto@users.noreply.github.com>
2022-02-03 16:16:58 -0300

    introJS-aula1

--- 7-condicionais.js
+++ 7-condicionais.js
@@ -7,22 +7,35 @@ const listaDeDestinos = new Array(
 );
 
 const idadeComprador = 17;
+const estaAcompanhada = true;
+const temPassagemComrada = true;
+
 console.log(" Destiinos possíveis");
 console.log(listaDeDestinos);
 
-if (maiorDeIdade >= 18) {
+if (maiorDeIdade >= 18) || (estaAcompanhada == true) {
     console.log("Comprador maior de idade");
+    console.log("Boa viagem");
     listaDeDestinos.splice(1, 1); //removendo item 
 } else {
-
     console.log("Não é maior de Idade e não  é possível a venda");
+}
+if {
+    console.log("Comprador maior de idade");
+}
+
+console.log("Embarque : \n\n")
+if (idadeComprador >= 18 && temPassagemComrada) {
+    console.log.apply("Boa Viagem");
+} else {
+    console.log("Você não pode embarcar")
 }
 
 
 console.log(listaDeDestinos);
 
-console.log(idadeComprador > 18);
-console.log(idadeComprador < 18);
-console.log(idadeComprador <= 18);
-console.log(idadeComprador >= 18);
-console.log(idadeComprador == 18);
+// console.log(idadeComprador > 18);
+// console.log(idadeComprador < 18);
+//console.log(idadeComprador <= 18);
+//console.log(idadeComprador >= 18);
+//console.log(idadeComprador == 18);
---
+++ 8-loops.js
@@ -0,0 +1,26 @@
+console.log(`\nTranlhando com codicionais`);
+const listaDeDestinos = new Array(
+    `salvador`,
+    `São Paulo`,
+    `Rio de Janiero`
+);
+
+const idadeComprador = 17;
+const estaAcompanhada = false;
+let temPassagemComprada = false;
+const destino = "Salvador";
+
+console.log("\nDestiinos possíveis");
+console.log(listaDeDestinos);
+
+const podeComprar maiorDeIdade >= 18 || estaAcompanhada == true;
+
+let contador = 0
+while (contador < 3) {
+    sonsolo.log(listaDeDestinos[contador == destino]; {
+            console.log("Destino Existe");
+        } else {
+            console.log("Destino Não existe");
+        }
+        contador += 1
+    }
